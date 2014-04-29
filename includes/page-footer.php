cc50d733fbb00152c36935c37a64aeef643073d5
Patrick Johnson <pbj@Patricks-MacBook-Pro.local>
2014-04-29 02:39:19 -0400

    updated nav/footer links

--- includes/page-footer.php
+++ includes/page-footer.php
@@ -8,7 +8,7 @@
 						</a>
 	    		</div>
 	    		<div class="col-md-3 col-xs-12 col-md-offset-1">
-	    			<a href="#" class="footer-section-title">About Us</a>
+	    			<a href="<?php echo BASE; ?>/about" class="footer-section-title">About Us</a>
 	    			<p>We create easy-to-use, cloud-based software that helps medical professionals analyze raw primary medical data at the point of diagnosis or evaluation.</p>
 	    		</div>
 	    		<div class="col-md-3 col-xs-12 footer-menu">
@@ -16,25 +16,25 @@
 	    			<div class="row">
 	    				<ul>
 	    					<li>
-	    						<a href="#">About Us</a>
+	    						<a href="<?php echo BASE; ?>/about">About Us</a>
 	    						<ul class="footer-menu-sub-nav">
-	    							<li><a href="#">Team</a></li>
-	    							<li><a href="#">Join</a></li>
+	    							<li><a href="<?php echo BASE; ?>/team">Team</a></li>
+	    							<li><a href="<?php echo BASE; ?>/join">Join</a></li>
 	    						</ul>
 	    					</li>
-	    					<li><a href="#">Technology</a></li>
-	    					<li><a href="#">Resources</a></li>
+	    					<li><a href="<?php echo BASE; ?>/technology">Technology</a></li>
+	    					<li><a href="<?php echo BASE; ?>/resources">Resources</a></li>
 	    				</ul>
 	    				<ul>
 	    					<li>
 	    						<a href="#">Products</a>
 	    						<ul class="footer-menu-sub-nav">
-	    							<li><a href="#">Neurology</a></li>
-	    							<li><a href="#">Imaging</a></li>
-	    							<li><a href="#">Genomics</a></li>
+	    							<li><a href="<?php echo BASE; ?>/neurology">Neurology</a></li>
+	    							<li><a href="<?php echo BASE; ?>/imaging">Imaging</a></li>
+	    							<li><a href="<?php echo BASE; ?>/genomics">Genomics</a></li>
 	    						</ul>
 	    					</li>
-	    					<li><a href="#">Contact Us</a></li>
+	    					<li><a href="<?php echo BASE; ?>/contact">Contact Us</a></li>
 	    				</ul>
 	    			</div>
 	    			
@@ -45,8 +45,8 @@
 	    		<div class="footer-company center">
 	    			<ul>
 	    				<li>2014 &copy; Picofemto</li>
-	    				<li><a href="#">Terms &amp; Conditions</a></li>
-	    				<li><a href="#">Privacy Policy</a></li>
+	    				<li><a href="<?php echo BASE; ?>/terms">Terms &amp; Conditions</a></li>
+	    				<li><a href="<?php echo BASE; ?>/privacy">Privacy Policy</a></li>
 	    			</ul>
 	    		</div>
 	    	</div>
