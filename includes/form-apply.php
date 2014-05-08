3e1c2a1760a6c108a3eab7768dfe735346609c41
Patrick Johnson <im@pbj.me>
2014-05-08 02:08:13 -0400

    updated job application form

--- includes/form-apply.php
+++ includes/form-apply.php
@@ -1,28 +1,22 @@
 <?php 
-<<<<<<< HEAD
   
   // TODO
   // - set up proper colon formatting for dates
+  // - set up proper time zone - bluehost?
   // - mail all contents to proper address
-  // - test email / html email
 
 
   // the header is making the success throw an error
   // but the email still sends. why?
-  
-  date_default_timezone_set('Etc/UTC');
+  date_default_timezone_set('America/New_York');
   header ("content-type: application/json; charset=utf-8");
   require '../includes/phpmailer/PHPMailerAutoload.php';
 
-  // arrays to store pass/fall values
-  //pass
-  $data = array();
 
-  //fail
+  $data = array();
   $errors = array();
 
 
-  // if the name doens't exist, give the error and store it to the errors array
   if (empty($_POST['name'])){
     $errors['name'] = "Name is required";
   } 
@@ -35,7 +29,6 @@
     $errors['message'] = "Message is required";
   } 
 
-  // if there are no files, give the error and store it to the errors array
   if (empty($_FILES)){
     $errors['file'] = "Resume is required";  
   }
@@ -48,8 +41,7 @@
     
   }
 
-  // else, if there isn't anyting in the errors array, continue with the upload 
-  // php mailer in here!
+  // else, send email, then upload to ../includes/job_applicants
   else {
     $data['success'] = true;
     $data['message'] = "Success! If we feel you're a fit we'll reach out shortly.";
@@ -58,9 +50,7 @@
     $fileUpload = $_FILES[file_0]['tmp_name'];
     $fileName = $_FILES[file_0]['name'];
 
-    // echo $test;
-
-    ///// run php mailer first
+    // php mailer
     $file = $_FILES["upload"]["tmp_name"];
       $name =     filter_var($_POST["name"], FILTER_SANITIZE_STRING);
       $email =    filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
@@ -115,7 +105,6 @@
       //Read an HTML message body from an external file, convert referenced images to embedded,
       //convert HTML into a basic plain-text alternative body
       $mail->Body = $message;
-      // $mail->MsgHTML($message);
 
       //Replace the plain text body with one created manually
       $mail->AltBody = 'This is a plain-text message body';
@@ -144,45 +133,5 @@
   }
 
 echo json_encode($data, JSON_PRETTY_PRINT);
-  
-
-=======
-	
-	echo "I'm here";
-	// $email and $message are the data that is being
-	// posted to this page from our html contact form
-	$email = $_POST['email'] ;
-	$message = $_POST['message'] ;
-
-	// When we unzipped PHPMailer, it unzipped to
-	// public_html/PHPMailer_5.2.0
-	require("phpmailer/class.phpmailer.php");
-
-	$mail = new PHPMailer();
-
-	$mail->IsSMTP();
-	$mail->SMTPAuth = true;
-	$mail->SMTPSecure = "tls";                 // sets the prefix to the servier
-	$mail->Host       = "smtp.gmail.com";      // sets GMAIL as the SMTP server
-	$mail->Port       = 587;  
-	$mail->Username 	= "pbjtest9@gmail.com";
-	$mail->Password 	= "pbjtest!";
-
-	$mail->SetFrom('name@yourdomain.com', 'First Last');
-	$mail->AddAddress("im@pbj.me","First Last")
-
-	$mail->Subject    = "PHPMailer Test Subject via smtp (Gmail), basic";
-	$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
-	$mail->MsgHTML($body);
-
-	$mail->AddAttachment("images/phpmailer.gif");      // attachment
-	$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
-	 
-	if(!$mail->Send()) {
-		echo "Mailer Error: " . $mail->ErrorInfo;
-	} else {
-		echo "Message sent!";
-	}
->>>>>>> 4b7de1681d1ed0bd75e12d967c614098888c32b7
 
 ?>
