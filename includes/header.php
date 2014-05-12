1f30f45039a59cac29382404f2ba431ca1759e6f
Patrick Johnson <im@pbj.me>
2014-05-11 22:48:57 -0400

    header update for humans.txt

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,17 @@
-
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <meta name="description" content="Picofemto creates easy-to-use, cloud-based software that helps medical professionals analyze raw primary medical data at the point of diagnosis or evaluation.">
+  <meta name="author" content="Picofemto">
   <link rel="shortcut icon" href="<?php echo BASE_URL; ?>assets/ico/favicon.ico">
+  <link rel="author" href="humans.txt" />
 
   <title>Picofemto</title>
 
@@ -19,10 +23,6 @@
 
   <!-- Modernizr -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js"></script>
-
-  <!-- Just for debugging purposes. Don't actually copy this line! -->
-  <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
